3ba2ae20ad03274be0338608763abb14becc9b22
Mike Pirog <mike@kalamuna.com>
2015-02-18 22:11:32 -0800

    Actually get options passed in as well

--- index.js
+++ index.js
@@ -6,6 +6,27 @@ module.exports = function(argv, app, events, engine, tasks) {
 
   // Helpers
   /**
+   * Returns an arrayed set of git-ready commands
+   **/
+  var getCmd = function() {
+    // @todo: not sure if the command structure is different on D7 vs D6
+    // Grab our options from config so we can filter these out
+    var cmd = argv._;
+    delete argv._;
+
+    for (var opt in argv) {
+      if (argv[opt] === true) {
+        cmd.push('--' + opt);
+      }
+      else {
+        cmd.push('--' + opt + '=' + argv[opt]);
+      }
+    }
+
+    return cmd;
+  };
+
+  /**
    * Runs a git command on the app data container
    **/
   var runGitCMD = function(cmd, done) {
@@ -48,7 +69,7 @@ module.exports = function(argv, app, events, engine, tasks) {
   tasks.registerTask([app.name, 'git'], function(done) {
     // We need to use this faux bin until the resolution of
     // https://github.com/syncthing/syncthing/issues/1056
-    var cmd = ['kgit'].concat(argv._);
+    var cmd = getCmd();
     runGitCMD(cmd, done);
   });
 
